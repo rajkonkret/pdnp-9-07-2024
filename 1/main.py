5a28d9b102e4ef9f7770983e4df3d8c712ca8bc8
rajkonkret <rajkonkret660@gmail.com>
2024-07-09 10:49:23 +0200

    init commit

---
+++ 1/main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# dwa razy shift - podręczne menu
+# wheel - powiekszanie literek kółkiem myszy
+# theme - zmiana wyglądu środowiska
+# ctrl alt l - formatowanie kodu
+# Hi, PyCharm
+# (venv) PS C:\Users\CSComarch\PycharmProjects\pdnp-9-07-2024> python.exe .\main.py
+# Hi, PyCharm
+# (venv) PS C:\Users\CSComarch\PycharmProjects\pdnp-9-07-2024>
+# ctrl / (?) - komentowanie zanzaczonych linijek
