9b282337fcb46d7c2b15e88e4ec32967e718cb27
rajkonkret <rajkonkret660@gmail.com>
2024-07-09 10:59:57 +0200

    [1.2] add pierwszy.py

---
+++ 1/pierwszy.py
@@ -0,0 +1,15 @@
+print()  # wypisz/wydrukuj
+print("Nazywam się Radek")
+print('Nazywam się Radek')
+# print("Nazywam się Radek')
+#   File "C:\Users\CSComarch\PycharmProjects\pdnp-9-07-2024\1\pierwszy.py", line 4
+#     print("Nazywam się Radek')
+#           ^
+# SyntaxError: unterminated string literal (detected at line 4)
+print("Nazywam się Radek")
+print("Nazywam się Radek")
+print("Nazywam się Radek")
+print("Nazywam się Radek")
+print("Nazywam się Radek")
+print("Nazywam się Radek")
+# ctrl d - powielanie zaznaczonych elementów
