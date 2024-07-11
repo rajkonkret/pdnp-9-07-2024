ef02b3585fbabd41df1bf65e76cdc6eeb237ab08
rajkonkret <rajkonkret660@gmail.com>
2024-07-11 09:32:14 +0200

    [3.2] update discount.py

--- 2/discount.py
+++ 2/discount.py
@@ -29,3 +29,25 @@ data_object = datetime.strptime("10/07/2024", '%d/%m/%Y')
 print(data_object)  # 2024-07-10 00:00:00
 print(type(data_object))  # <class 'datetime.datetime'>
 
+products = [
+    {'sku': 1, 'exp_date': today, 'price': 100},
+    {'sku': 2, 'exp_date': tomorrow, 'price': 200},
+    {'sku': 3, 'exp_date': today, 'price': 50.00},
+    {'sku': 4, 'exp_date': tomorrow, 'price': 125},
+    {'sku': 5, 'exp_date': today, 'price': 74.99},
+]
+
+for p in products:
+    # print(p)  # {'sku': 1, 'exp_date': datetime.date(2024, 7, 11), 'price': 100}
+    # print(p['exp_date'])
+    if p['exp_date'] != today:
+        continue  # wraca na początek pętli, bierze kolejny element
+    p['price'] *= 0.8  # price = price * 0.8
+    print(f"""Price for sku {p['sku']}
+is now {p['price']}""")
+# Price for sku 1
+# is now 80.0
+# Price for sku 3
+# is now 40.0
+# Price for sku 5
+# is now 59.992
