c332ca2ef4b175a97ef5a2d8d4d7ab5bf91095e9
rajkonkret <rajkonkret660@gmail.com>
2024-07-12 13:16:16 +0200

    [4.5] add matematyka.py

---
+++ 4/matematyka.py
@@ -0,0 +1,8 @@
+import math
+
+print(math.pi)  # 3.141592653589793
+angle_degrees = 90
+angle_radians = math.radians(angle_degrees)
+print(math.cos(angle_radians))  # 6.123233995736766e-17
+
+print(math.sqrt(25))  # 5.0
